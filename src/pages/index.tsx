5c897b4a7c2e7dc20be473bd283742bc39f01e26
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-01 19:12:31 +0100

    pokemon list is now sorted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,7 @@ const Home: NextPage = () => {
       ).then((response) => response.json());
 
       setPokemons(newPokemon);
+      pokemon.sort((a, b) => a.id - b.id);
     });
   }
 
@@ -68,7 +69,7 @@ const Home: NextPage = () => {
         endMessage={<h4>Nothing more to show</h4>}
         className="flex flex-col items-center justify-center"
       >
-        <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-4 p-16 xl:w-1/2 w-full">
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 sm:grid-cols-1 gap-4 p-16 xl:w-2/3 w-full">
           {pokemon.map((data: IPokemon, idx: number) => (
             <PokemonCard
               key={idx}
