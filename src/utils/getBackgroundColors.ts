12c74f512868142aa5924df7767274b842905b66
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-02 21:56:28 +0100

    outsourced functions

---
+++ src/utils/getBackgroundColors.ts
@@ -0,0 +1,15 @@
+import { Type } from "types/Pokemon";
+import { ColorProps } from "types/types";
+import { PokemonTypeColor } from "./colors";
+
+export function getBackgroundColors(
+  type: Type[]
+): { light: string; medium: string }[] {
+  return type.map(({ type }) => {
+    const [[, backgroundColor]] = Object.entries(PokemonTypeColor).filter(
+      ([key, _]) => key === type.name
+    );
+
+    return backgroundColor;
+  });
+}
