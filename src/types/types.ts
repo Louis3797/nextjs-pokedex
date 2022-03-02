6125d213042b73b91c2ed077e0a8c904853ed2cb
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-02 21:54:24 +0100

    extra color type in type dir

---
+++ src/types/types.ts
@@ -0,0 +1,74 @@
+export interface ColorProps {
+  normal: {
+    light: string;
+    medium: string;
+  };
+  fire: {
+    light: string;
+    medium: string;
+  };
+  fighting: {
+    light: string;
+    medium: string;
+  };
+  water: {
+    light: string;
+    medium: string;
+  };
+  flying: {
+    light: string;
+    medium: string;
+  };
+  grass: {
+    light: string;
+    medium: string;
+  };
+  poison: {
+    light: string;
+    medium: string;
+  };
+  electric: {
+    light: string;
+    medium: string;
+  };
+  ground: {
+    light: string;
+    medium: string;
+  };
+  psychic: {
+    light: string;
+    medium: string;
+  };
+  rock: {
+    light: string;
+    medium: string;
+  };
+  ice: {
+    light: string;
+    medium: string;
+  };
+  bug: {
+    light: string;
+    medium: string;
+  };
+  dragon: {
+    light: string;
+    medium: string;
+  };
+  ghost: {
+    light: string;
+    medium: string;
+  };
+  dark: {
+    light: string;
+    medium: string;
+  };
+  steel: {
+    light: string;
+    medium: string;
+  };
+  fairy: {
+    light: string;
+    medium: string;
+  };
+}
