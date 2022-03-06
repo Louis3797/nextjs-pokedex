8447e073fab0528163695b972d0c532aaf7edbfc
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-06 21:44:31 +0100

    added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "@/components/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Layout>
         <Component {...pageProps} />;
       </Layout>
