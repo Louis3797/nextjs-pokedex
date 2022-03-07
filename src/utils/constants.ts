fbdad4de6a9580e7073ff7a8aef8423c7f431365
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-07 22:23:38 +0100

    changes url for images, .webp now

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const IMG_URL: string =
-  "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/";
+  "https://raw.githubusercontent.com/anurag-roy/poke-api/main/assets/images/";
