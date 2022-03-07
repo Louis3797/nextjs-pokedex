ece4bf6dd8fa34cb3eae3d2341f3ca12214294aa
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-07 22:24:20 +0100

    small changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="assets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="assets/favicon-16x16.png"
+        />
+        <link rel="shortcut icon" href="./assets/favicon.ico" />
+        <link
+          rel="mask-icon"
+          href="assets/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Layout>
