f8eb69493217e8573504ceea4197fab38ea84820
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-08 22:21:27 +0100

    updated mobile ui

--- src/components/PokemonImage.tsx
+++ src/components/PokemonImage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-key */
 import { usePokemonDetailStore } from "global-stores/PokemonDetailStore";
 import React, { useMemo } from "react";
@@ -24,32 +25,30 @@ export const PokemonImage = () => {
 
   return (
     <div
-      className="flex flex-col h-full lg:w-1/2 w-full relative lg:rounded-l-2xl rounded-t-2xl lg:justify-center justify-end items-center"
+      className="flex flex-col h-full lg:w-1/2 w-full relative lg:rounded-l-2xl rounded-t-2xl justify-start items-center"
       style={{
         background: `radial-gradient(#fafafa,30%, ${backgroundColors[0].medium})`,
       }}
     >
-      <div className="justify-start items-start w-full py-4 px-5 top-0 absolute">
+      <div className="justify-start items-start w-full py-4 px-5">
         <Link href={"/"} passHref>
           <BsArrowLeftShort className="text-4xl text-primary mb-1 opacity-80 font-light hover:text-secondary" />
         </Link>
-        <p className="text-xl font-medium text-primary px-2">
+        <p className="md:text-xl text-lg font-medium text-primary px-2">
           {"#" + pokemon.id.toString().padStart(3, "0")}
         </p>
-        <p className="text-3xl font-semibold  text-primary px-2">
-          {capitalizeFirstLetter(pokemon.name)}
+        <p className="md:text-3xl text-2xl font-semibold text-primary px-2 capitalize">
+          {pokemon.name}
         </p>
       </div>
-      <p className="font-bold text-7xl drop-shadow-xl tracking-widest absolute text-primary opacity-60 top-32  lg:top-44">
+      <p className="font-bold md:text-7xl text-5xl drop-shadow-xl tracking-widest absolute text-primary opacity-60 top-32 lg:top-44">
         {jpName?.name}
       </p>
-      <Image
+      <img
         key={pokemon.id}
         src={`${IMG_URL + pokemon.id}.webp`}
         alt={pokemon.name}
-        height={350}
-        width={350}
-        className="animate-poke-bounce"
+        className="animate-poke-bounce h-52 w-52 md:w-96 md:h-96 md:mt-20"
       />
     </div>
   );
