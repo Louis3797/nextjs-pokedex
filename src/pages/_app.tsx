8bfec33ca7cecd57a2317b4e1a301fe31fe54252
Louis <53270473+Louis3797@users.noreply.github.com>
2022-03-28 19:51:14 +0200

    fixed bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Layout>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Layout>
     </>
   );
